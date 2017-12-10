03c4325feb7fc002225bb177e5953f697be8fd82
takearest21 <takearest21@gmail.com>
2017-12-10 23:24:21 +0800

    finish reset error msg when user retype correct username and password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { View, Text } from  'react-native';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
@@ -17,6 +18,18 @@ class LoginForm extends Component {
 
 		this.props.loginUser({email,password});
 	}
+	renderError() {
+		if(this.props.error) {
+			return (
+				<View style={{ backgroundColor: 'white'}}>
+					<Text style={ styles.errorTextStyle}>
+						{this.props.error}
+					</Text>
+				</View>
+			);
+		}
+	}
+
 	render() {
 		return (
 			<Card>
@@ -39,6 +52,8 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				{this.renderError()}
+
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>
 					Login
@@ -50,10 +65,19 @@ class LoginForm extends Component {
 	};
 }
 
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+}
+
 const mapStateToProps = state => {
 	return {
 		email: state.auth.email,
-		password: state.auth.password
+		password: state.auth.password,
+		error: state.auth.error
 	};
 };
 
