8bdc44fb5b7dd8035c9be953ef1424f05b067ed6
takearest21 <takearest21@gmail.com>
2017-12-12 00:57:01 +0800

    added add button in main page

--- src/Router.js
+++ src/Router.js
@@ -14,8 +14,12 @@ const RouterComponent = () => {
 				</Scene>
 
 				<Scene key="main">	
-					<Scene key="employeeList" component={EmployeeList} 
-							title="Employees" />
+					<Scene 
+						onRight={() => console.log("right")}
+						rightTitle="Add"
+						key="employeeList"
+						component={EmployeeList} 
+						title="Employees" />
 				</Scene>
 			</Scene>
 		</Router>
