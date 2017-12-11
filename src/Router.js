9c90cad390fb9686f7eecb2d8b5a1b40e33f870d
takearest21 <takearest21@gmail.com>
2017-12-12 00:51:47 +0800

    add router

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,20 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
 	return (
 		<Router>
 			<Scene key="root">
-				<Scene key="login" component={LoginForm} title="Login" />
+				<Scene key="auth">
+					<Scene key="login" component={LoginForm} title="Login" />
+				</Scene>
+
+				<Scene key="main">	
+					<Scene key="employeeList" component={EmployeeList} 
+							title="Employees" />
+				</Scene>
 			</Scene>
 		</Router>
 	);
