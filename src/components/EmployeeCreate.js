b2d962c1759b25aa4010fdde2947d562cd4512d9
tomtse <tomtse@onioncreative.com>
2017-12-14 16:58:18 +0800

    talk 147

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate ,employeeCreate} from '../actions';
 import { Card, CardSection, Input , Button } from './common';
 
 
 class EmployeeCreate extends Component {
+	onButtonPress(){
+		const { name, phone, shift} = this.props;
+
+		this.props.employeeCreate({ name, phone, shift : shift || 'Monday'});
+	}
+
 	render() {
 		return (
 			<Card>
@@ -45,7 +51,7 @@ class EmployeeCreate extends Component {
 				</CardSection>
 
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Create
 					</Button>
 				</CardSection>
@@ -65,4 +71,4 @@ const mapStateToPorps = (state) => {
 	return { name, phone, shift };
 };
 
-export default connect(mapStateToPorps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToPorps, { employeeUpdate ,employeeCreate})(EmployeeCreate);
